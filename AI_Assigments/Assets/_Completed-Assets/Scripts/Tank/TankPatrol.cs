75443b92b5fc43e33d45685351e5423feaf1220e
M1R4B3L <amirabelmartinez@gmail.com>
2020-11-05 14:02:50 +0100

    new version init de patrol, i'm just investigating

---
+++ AI_Assigments/Assets/_Completed-Assets/Scripts/Tank/TankPatrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TankPatrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   // public List<GameObject> patrol_points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+      
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+}
